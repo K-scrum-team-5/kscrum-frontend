0ec840f6d9d70fbd0113a71d1550b4717ea913fd
진우현 <@naver.com>
2024-04-30 09:40:18 +0900

    feed page and sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <genre-selection v-if="!selectedGenre" @genre-selected="onGenreSelected" />
-    <movie-feed v-else :genre="selectedGenre" />
-  </div>
+
+  <SideBar_Page/>
+  
+  <Container_Page :인스타데이터 = "인스타데이터"/>
+
+  <div class="footer">
+    
+ </div>
 </template>
 
 <script>
-import GenreSelection from './GenreSelection.vue';
-import MovieFeed from './MovieFeed.vue';
+import instaData from './assets/instaData';
+import Container_Page from './components/Container_Page.vue';
+import SideBar_Page from './components/SideBar_Page.vue';
 
 export default {
+  name: 'App',
+  data(){
+    return{
+      인스타데이터:instaData,
+    }
+  },
   components: {
-    GenreSelection,
-    MovieFeed,
-  },
-  data() {
-    return {
-      selectedGenre: null,
-    };
-  },
-  methods: {
-    onGenreSelected(genre) {
-      this.selectedGenre = genre;
-    },
-  },
-};
+   Container_Page,
+   SideBar_Page,
+  }
+}
 </script>
+
+<style>
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ src/components/Container_Page.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <Post_Page :인스타데이터 = "인스타데이터[i]" v-for="(a,i) in 인스타데이터" :key="i"/>
+</div>
+</template>
+
+<script>
+import Post_Page from './Post_Page.vue';
+
+export default {
+    components:{
+        Post_Page,
+    },
+    props:{
+        인스타데이터 : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
