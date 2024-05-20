208de73c156d5d75ab13fe02adbb80c3cd78ad5f
진우현 <@naver.com>
2024-05-20 23:08:45 +0900

    TC-67 다크모드 오류수정

--- src/components/GenreProfile.vue
+++ src/components/GenreProfile.vue
@@ -28,85 +28,72 @@
         <h3>{{ selectedMovie.original_title }}</h3>
         <p class="date">{{ selectedMovie.id }}</p>
       </div>
+      </div>
     </div>
   </div>
 </template>
-
-<script>
-import axios from 'axios';
-import { useRoute } from 'vue-router';
-import InfiniteLoading from 'vue-infinite-loading';
-
-export default {
-  components: {
-    InfiniteLoading,
-  },
-  setup() {
-    const route = useRoute();
-    const genreName = route.params.genreName;
-
-    return {
-      genreName,
-    };
-  },
-  data() {
-    return {
-      movies: [],
-      loading: false,
-      busy: false,
-      page: 0,
-      showModal: false,
-      selectedMovie: null,
-    };
-  },
-  mounted() {
-    this.fetchMovies();
-  },
-  methods: {
-    fetchMovies($state) {
-      this.loading = true;
-      const url = `http://49.50.174.94:8080/api/movie/genre?page=${this.page}&size=8&genre=${this.genreName}`;
-      axios.get(url)
-
+  
+  <script>
+  import axios from 'axios';
+  import { useRoute } from 'vue-router';
+  
+  export default {
+    setup() {
+      const route = useRoute();
+      const genreName = route.params.genreName;
+  
+      return {
+        genreName,
+      };
+    },
+    data() {
+      return {
+        movies: [],
+        loading: false,
+        busy: false,
+        page: 0,
+        showModal: false,
+        selectedMovie: null,
+      };
+    },
+    mounted() {
+      this.fetchMovies();
+    },
+    methods: {
+      fetchMovies() {
+        this.loading = true;
+        const url = `http://49.50.174.94:8080/api/movie/genre?page=${this.page}&size=8&genre=${this.genreName}`;
+        axios.get(url)
           .then(response => {
-            if (response.data.length) {
-              this.movies = [...this.movies, ...response.data];
-              this.page++;
-              if ($state) {
-                $state.loaded();
-              }
-            } else {
-              if ($state) {
-                $state.complete();
-              }
-            }
+            this.movies = [...this.movies, ...response.data];
             this.loading = false;
             this.busy = false;
+            this.page++;
           })
           .catch(error => {
             console.error(error);
-            if ($state) {
-              $state.complete();
-            }
             this.loading = false;
             this.busy = false;
           });
+      },
+      loadMore() {
+        this.busy = true;
+        setTimeout(() => {
+          this.fetchMovies();
+        }, 1000);
+      },
+      openModal(movie) {
+        this.showModal = true;
+        this.selectedMovie = movie;
+      },
+      closeModal(event) {
+        if (event.target === event.currentTarget) {
+          this.showModal = false;
+        }
+      },
     },
-    loadMore($state) {
-      this.fetchMovies($state);
-    },
-    openModal(movie) {
-      this.showModal = true;
-      this.selectedMovie = movie;
-    },
-    closeModal(event) {
-      if (event.target === event.currentTarget) {
-        this.showModal = false;
-      }
-    },
-  },
-};
-</script>
+  };
+  </script> 
 
 <style>
 
